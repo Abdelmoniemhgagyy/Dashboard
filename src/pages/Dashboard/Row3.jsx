cb164a8c3a9bcaec8909fa22ec50e6c9932df063
Abdelmoniemhgagyy <moniemhgagy351@gmail.com>
2023-11-25 18:19:16 +0200

    updata row3

--- src/pages/Dashboard/Row3.jsx
+++ src/pages/Dashboard/Row3.jsx
@@ -8,12 +8,12 @@ function Row3() {
   const theme = useTheme();
   return (
     <Stack direction={"row"} flexWrap={"wrap"} gap={2} sx={{textAlign:"center"}} >
-       <Paper sx={{flexGrow: 1,}}>
+       <Paper sx={{flexGrow: 1}}>
 
          <Typography sx={{color:theme.palette.secondary.main , fontSize:"24px",margin:"10px 15px"}}>
            Compaign
          </Typography>
-        <Box  width={"450px"} maxHeight={"300px"} sx={{margin:"0 auto"}} >
+        <Box  width={"430px"} maxHeight={"300px"} sx={{margin:"0 auto"}} >
                <Poe isDashboard={true} />   
         </Box>
 
@@ -22,7 +22,7 @@ function Row3() {
          <Typography sx={{color:theme.palette.secondary.main, fontSize:"24px",margin:"10px 15px"}}>
            Sales Quantity 
          </Typography>
-         <Box  width={"450px"} maxHeight={"300px"} sx={{margin:"0 auto"}}>
+         <Box  width={"430px"} maxHeight={"300px"} sx={{margin:"0 auto"}}>
              <Bar isDashboard={true}/>
         </Box>
 
@@ -31,7 +31,7 @@ function Row3() {
          <Typography sx={{color:theme.palette.secondary.main,fontSize:"24px",margin:"10px 15px"}}>
            Geography
          </Typography>
-         <Box  width={"450px"} maxHeight={"300px"} sx={{margin:"0 auto"}}>
+         <Box  width={"430px"} maxHeight={"300px"} sx={{margin:"0 auto"}}>
                  <Geo isDashboard={true}/>       
         </Box>
        
