ef5946e7a47c67aa56dc81a582f186fcc96d61e1
Abdelmoniemhgagyy <moniemhgagy351@gmail.com>
2023-11-25 19:13:07 +0200

    updata row1

--- src/pages/Dashboard/card.jsx
+++ src/pages/Dashboard/card.jsx
@@ -5,7 +5,7 @@ import { useTheme } from '@mui/material/styles';
 function Card({icon,count,title,increase,data,scheme}) {
   const theme = useTheme();
   return (
-    <Paper sx={{minWidth:"340px",display:"flex",justifyContent:"space-between",padding:"4px 5px",flexGrow: 1}} >
+    <Paper sx={{minWidth:"300px",display:"flex",justifyContent:"space-between",padding:"4px 5px",flexGrow: 1}} >
           <Stack direction={"column"} sx={{lineHeight:"1.8", textAlign:"center",padding:"5px"}}>
           <Box>{icon} </Box>  
             <Typography  variant="p" >
